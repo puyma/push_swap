4b69f0b78b4fd0c574edd885d16c4cb77acf8144
puyma <mpuig-ma@student.42barcelona.com>
2023-03-16 22:50:42 +0100

    simplified with ft_exit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 14:56:31 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/16 20:02:15 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/16 22:24:00 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,7 @@ int	main(int argc, char **argv)
 	a = ft_new_stack("a");
 	b = ft_new_stack("b");
 	if (ft_parse_arguments(a, argc, argv) == 0)
-	{
-		ft_printf("Error\n");
-		return (0);
-	}
+		ft_exit(0);
 	while (a->numbers != NULL)
 	{
 		ft_printf("%d\n", a->numbers->content);
