c9d63fbf865b9073b4750ca9f47a9cabb9494025
puyma <mpuig-ma@student.42barcelona.com>
2023-03-18 17:19:05 +0100

    reverse rotate OK

--- src/reverse_rotate.c
+++ src/reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 15:21:51 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/16 15:38:10 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/18 17:18:04 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int static	ft_reverse_rotate(t_stack *stack);
 // The last element becomes the first one.
 int	ft_rra(t_stack *a)
 {
+	write(1, "rra\n", 4);
 	ft_reverse_rotate(a);
 	return (0);
 }
@@ -28,6 +29,7 @@ int	ft_rra(t_stack *a)
 // The last element becomes the first one.
 int	ft_rrb(t_stack *b)
 {
+	write(1, "rrb\n", 4);
 	ft_reverse_rotate(b);
 	return (0);
 }
@@ -35,6 +37,7 @@ int	ft_rrb(t_stack *b)
 // rra and rrb at the same time.
 int	ft_rrr(t_stack *a, t_stack *b)
 {
+	write(1, "rrr\n", 4);
 	ft_rra(a);
 	ft_rrb(b);
 	return (0);
@@ -42,6 +45,13 @@ int	ft_rrr(t_stack *a, t_stack *b)
 
 static int	ft_reverse_rotate(t_stack *stack)
 {
-	(void) stack;
+	t_list	*last;
+
+	if (stack->numbers == NULL)
+		return (0);
+	last = ft_lstlast(stack->numbers);
+	last->prev->next = NULL;
+	ft_lstadd_front(&stack->numbers, last);
+	stack->numbers = last;
 	return (0);
 }
