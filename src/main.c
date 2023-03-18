b0aaba573c3189522121cc843dfaacde6bb9ce66
puyma <mpuig-ma@student.42barcelona.com>
2023-03-18 17:57:05 +0100

    main calls logic

--- src/main.c
+++ src/main.c
@@ -6,14 +6,14 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 14:56:31 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/18 17:18:12 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/18 17:50:51 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	ft_print_stack(t_stack *stack, int c);
-static void	ft_print_stack_rev(t_stack *stack, int c);
+static void	ft_print_stacks(t_stack *a, t_stack *b);
+static void	ft_print_stacks_rev(t_stack *a, t_stack *b);
 
 int	main(int argc, char **argv)
 {
@@ -28,40 +28,58 @@ int	main(int argc, char **argv)
 		return (0);
 	if (ft_parse_arguments(a, argc, argv) == 0)
 		ft_exit(0);
-	ft_print_stack(a, 'A');
-	ft_rra(a);
-	ft_print_stack(a, 'A');
+	ft_do_logic(a, b);
+	ft_print_stacks(a, b);
+	ft_print_stacks_rev(a, b);
 	return (0);
-	(void) ft_print_stack_rev;
 }
 
-static void	ft_print_stack(t_stack *stack, int c)
+static void	ft_print_stacks(t_stack *a, t_stack *b)
 {
-	t_list	*l;
+	t_list	*la;
+	t_list	*lb;
 
-	l = stack->numbers;
-	write(1, "-- ", 3);
-	write(1, &c, 1);
-	write(1, "\n", 1);
-	while (l != NULL)
+	la = a->numbers;
+	lb = b->numbers;
+	write(1, "---------\n", 10);
+	while (la != NULL || lb != NULL)
 	{
-		ft_printf("%d\n", l->content);
-		l = l->next;
+		if (la)
+			ft_printf("%d", la->content);
+		write(1, "\t", 1);
+		if (lb)
+			ft_printf("%d", lb->content);
+		write(1, "\n", 1);
+		if (la != NULL)
+			la = la->next;
+		if (lb != NULL)
+			lb = lb->next;
 	}
-	write(1, "--\n", 3);
+	write(1, "-\t-\n", 4);
+	write(1, "A\tB\n", 4);
 }
 
-static void	ft_print_stack_rev(t_stack *stack, int c)
+static void	ft_print_stacks_rev(t_stack *a, t_stack *b)
 {
-	t_list	*l;
+	t_list	*la;
+	t_list	*lb;
 
-	l = ft_lstlast(stack->numbers);
-	write(1, "-- ", 3);
-	write(1, &c, 1);
-	write(1, "\n", 1);
-	while (l != NULL)
+	la = ft_lstlast(a->numbers);
+	lb = ft_lstlast(b->numbers);
+	write(1, "---------(rev)\n", 16);
+	while (la != NULL || lb != NULL)
 	{
-		ft_printf("%d\n", l->content);
-		l = l->prev;
+		if (la)
+			ft_printf("%d", la->content);
+		write(1, "\t", 1);
+		if (lb)
+			ft_printf("%d", lb->content);
+		write(1, "\n", 1);
+		if (la != NULL)
+			la = la->prev;
+		if (lb != NULL)
+			lb = lb->prev;
 	}
+	write(1, "-\t-\n", 4);
+	write(1, "A\tB\n", 4);
 }
