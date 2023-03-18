e470e7b875330a866287b74043978f38a9cac8d7
puyma <mpuig-ma@student.42barcelona.com>
2023-03-18 15:52:49 +0100

    rotate OK

--- src/rotate.c
+++ src/rotate.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 13:05:59 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/16 15:39:49 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/18 15:51:05 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ static int	ft_rotate(t_stack *stack);
 // The first element becomes the last one.
 int	ft_ra(t_stack *a)
 {
+	write(1, "ra\n", 3);
 	ft_rotate(a);
 	return (0);
 }
@@ -28,6 +29,7 @@ int	ft_ra(t_stack *a)
 // The first element becomes the last one.
 int	ft_rb(t_stack *b)
 {
+	write(1, "rb\n", 3);
 	ft_rotate(b);
 	return (0);
 }
@@ -35,6 +37,7 @@ int	ft_rb(t_stack *b)
 // ra and rb at the same time.
 int	ft_rr(t_stack *a, t_stack *b)
 {
+	write(1, "rr\n", 3);
 	ft_ra(a);
 	ft_rb(b);
 	return (0);
@@ -42,6 +45,10 @@ int	ft_rr(t_stack *a, t_stack *b)
 
 static int	ft_rotate(t_stack *stack)
 {
-	(void) stack;
+	if (stack->numbers == NULL)
+		return (0);
+	stack->numbers = stack->numbers->next;
+	ft_lstadd_back(&stack->numbers, stack->numbers->prev);
+	stack->numbers->prev = NULL;
 	return (0);
 }
