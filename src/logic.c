4b0cb5801a04d62ee630cba2ca3d752484d17d41
puyma <mpuig-ma@student.42barcelona.com>
2023-03-18 18:08:55 +0100

    subject tests

--- src/logic.c
+++ src/logic.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 17:29:39 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/18 17:51:53 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/18 18:04:57 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,15 @@ int	ft_do_logic(t_stack *a, t_stack *b)
 	ft_sa(a);
 	ft_pb(a, b);
 	ft_pb(a, b);
-	//ft_pb(a, b);
+	ft_pb(a, b);
+	ft_ra(a);
+	ft_rb(b);
+	ft_rra(a);
+	ft_rrb(b);
+	ft_sa(a);
+	ft_pa(a, b);
+	ft_pa(a, b);
+	ft_pa(a, b);
 	return (0);
 	(void) a;
 	(void) b;
