e17b9c064f321d325e589ffeaacfc5b9d499c113
puyma <mpuig-ma@student.42barcelona.com>
2023-03-19 21:05:00 +0100

    LOG mode && data_init

--- src/data.c
+++ src/data.c
@@ -6,20 +6,22 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 18:17:10 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/19 18:19:09 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/19 18:29:54 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_init_data(t_data *data)
+t_data	*ft_init_data(void)
 {
+	t_data	*data;
+
 	data = ft_calloc(1, sizeof(t_data));
 	if (data == NULL)
-		return (0);
+		return (NULL);
 	data->a = ft_calloc(1, sizeof(t_stack));
 	data->b = ft_calloc(1, sizeof(t_stack));
 	if (data->a == NULL || data->b == NULL)
-		return (0);
-	return (1);
+		return (NULL);
+	return (data);
 }
--- src/push_swap.h
+++ src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 15:04:06 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/19 18:19:02 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/19 21:01:33 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,14 @@
 # include <stdlib.h> /* malloc, free, exit */
 # include "libft.h" /* ft_printf, ... */
 
-# define FD		1
+# define FD			1
+# define LOG		1
+# define DIRECTION	1
 
 typedef struct s_list
 {
 	int				content;
+	int				index;
 	struct s_list	*next;
 	struct s_list	*prev;
 }					t_list;
@@ -36,11 +39,14 @@ typedef struct s_data
 {
 	t_stack			*a;
 	t_stack			*b;
+	int				size;
 }					t_data;
 
-int		ft_init_data(t_data *data);
+t_data	*ft_init_data(void);
 int		ft_parse_arguments(t_data *data, int argc, char **argv);
 int		ft_do_logic(t_data *data);
+void	ft_exit(int n);
+void	ft_print_stacks(t_data *data, int dir);
 
 // swap
 int		ft_sa(t_data *data);
@@ -66,7 +72,5 @@ void	ft_lstadd_back(t_list **lst, t_list *new);
 t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstnew(int content);
 int		ft_lstsize(t_list *lst);
-
-void	ft_exit(int n);
 
 #endif /* push_swap.h */
