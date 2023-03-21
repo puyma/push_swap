e306637b79385bec197d2d6c1899aab05dbde49f
puyma <mpuig-ma@student.42barcelona.com>
2023-03-21 15:43:53 +0100

    checker main OK

--- src/main_bonus.c
+++ src/main_bonus.c
@@ -6,19 +6,21 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 14:56:31 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/21 14:51:55 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/21 15:38:26 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	ft_check_logic(void);
-static int	ft_read_instructions(void);
+static t_list	*ft_read_instructions(void);
 
 int	main(int argc, char **argv)
 {
 	t_data	*data;
+	t_list	*instructions;
 
+	data = NULL;
+	instructions = NULL;
 	if (argc < 2)
 		return (0);
 	data = ft_init_data();
@@ -26,28 +28,28 @@ int	main(int argc, char **argv)
 		ft_exit(0);
 	if (ft_parse_arguments(data, argc, argv) == 0)
 		ft_exit(0);
-	ft_read_instructions();
-	if (ft_check_logic() == 0)
+	instructions = ft_read_instructions();
+	if (instructions == NULL)
+		ft_exit(0);
+	if (ft_check_logic(data, instructions) == 0)
 		ft_printf("KO\n");
 	else
 		ft_printf("OK\n");
 	return (0);
 }
 
-static int	ft_read_instructions(void)
+static t_list	*ft_read_instructions(void)
 {
-	return (0);
-}
+	t_list	*list;
+	char	*line;
 
-// read from stdin until ^D or EOF
-// if coming from ./push_swap, EOF is automatic
-// else, is users ^D
-//
-// then, parse instructions. only a few are valid
-// else, "Error\n"
-//
-
-static int	ft_check_logic(void)
-{
-	return (1);
+	list = NULL;
+	line = NULL;
+	line = get_next_line(STDIN_FILENO);
+	while (line != NULL && *line != '\0')
+	{
+		ft_lstadd_back(&list, ft_lstnew_str(line));
+		line = get_next_line(STDIN_FILENO);
+	}
+	return (list);
 }
