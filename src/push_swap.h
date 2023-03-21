dd42121d0e5495940ac33e614c8b6232588a2514
puyma <mpuig-ma@student.42barcelona.com>
2023-03-21 15:43:24 +0100

    declared new functions

--- src/push_swap.h
+++ src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 15:04:06 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/21 12:04:00 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/21 15:38:03 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ enum e_direction { backwards = 0, forward }	direction;
 typedef struct s_list
 {
 	int				content;
-	int				index;
+	char			*content_s;
 	struct s_list	*next;
 	struct s_list	*prev;
 }					t_list;
@@ -45,35 +45,43 @@ typedef struct s_data
 	int				chunk_size;
 }					t_data;
 
+// push_swap functions:
+
 t_data	*ft_init_data(void);
 int		ft_parse_arguments(t_data *data, int argc, char **argv);
 int		ft_do_logic(t_data *data);
-void	ft_exit(int n);
 void	ft_print_stacks(t_data *data, int dir);
+void	ft_exit(int n);
+int		ft_issorted(t_data *data);
 
-// swap
+// checker functions:
+
+int		ft_check_logic(t_data *data, t_list *instructions);
+
+// Instructions: push, rotate, 
+// reverse rotate and swap.
+
+int		ft_pa(t_data *data);
+int		ft_pb(t_data *data);
+int		ft_ra(t_data *data);
+int		ft_rb(t_data *data);
+int		ft_rr(t_data *data);
+int		ft_rra(t_data *data);
+int		ft_rrb(t_data *data);
+int		ft_rrr(t_data *data);
 int		ft_sa(t_data *data);
 int		ft_sb(t_data *data);
 int		ft_ss(t_data *data);
 
-// push
-int		ft_pa(t_data *data);
-int		ft_pb(t_data *data);
-
-// rotate
-int		ft_ra(t_data *data);
-int		ft_rb(t_data *data);
-int		ft_rr(t_data *data);
-
-// reverse rotate
-int		ft_rra(t_data *data);
-int		ft_rrb(t_data *data);
-int		ft_rrr(t_data *data);
+// List functions:
 
 void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstadd_back(t_list **lst, t_list *new);
-t_list	*ft_lstlast(t_list *lst);
+size_t	ft_lstsize(t_list *lst);
+size_t	ft_lst_position(t_list *list, t_list *node);
+t_list	*ft_lstnew_str(char *content_s);
 t_list	*ft_lstnew(int content);
-int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_search_from(t_data *data, int chunk, int dir);
 
 #endif /* push_swap.h */
