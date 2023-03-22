0a562d387ec75d0f2c58e0d2aa2145dc42f51d24
puyma <mpuig-ma@student.42barcelona.com>
2023-03-22 13:51:22 +0100

    missing func parser, OK

--- src/logic_bonus.c
+++ src/logic_bonus.c
@@ -6,23 +6,26 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 17:29:39 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/21 15:47:59 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/22 13:50:50 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// read from stdin until ^D or EOF
-// if coming from ./push_swap, EOF is automatic
-// else, is users ^D
-//
-// then, parse instructions. only a few are valid
-// else, "Error\n"
-//
-
 int	ft_check_logic(t_data *data, t_list *instructions)
 {
-	(void) data;
-	(void) instructions;
+	t_list	*l;
+	int		*do_instruction(t_data *data);
+
+	l = instructions;
+	while (l != NULL)
+	{
+		do_instruction = ft_set_func(l->content_s);
+		if (do_instruction != NULL)
+			do_instruction(data);
+		l = l->next;
+	}
+	if (ft_issorted(data) == 0)
+		return (0);
 	return (1);
 }
