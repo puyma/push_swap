36904b3134e68d2a6018edb7271651083b38a944
puyma <mpuig-ma@student.42barcelona.com>
2023-03-23 14:31:02 +0100

    rm enum

--- src/lists_other.c
+++ src/lists_other.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 15:24:04 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/22 16:38:35 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/23 11:15:16 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list	*ft_search_from(t_data *data, int chunk, int dir)
 	t_list	*l;
 	t_list	*node;
 
-	if (dir == backwards)
+	if (dir == -1)
 		l = ft_lstlast(data->a->numbers);
 	else
 		l = data->a->numbers;
@@ -26,7 +26,7 @@ t_list	*ft_search_from(t_data *data, int chunk, int dir)
 	{
 		if (l->content < chunk)
 			return (l);
-		if (dir == backwards)
+		if (dir == -1)
 			l = l->prev;
 		else
 			l = l->next;
