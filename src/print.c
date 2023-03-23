f5ed24e281eea5ab3ceaf0d994afe693b2451542
puyma <mpuig-ma@student.42barcelona.com>
2023-03-23 14:30:17 +0100

    default print reverse

--- src/print.c
+++ src/print.c
@@ -6,24 +6,23 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 18:53:27 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/19 19:06:37 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/23 11:21:21 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 static t_list	*ft_iter_direction(t_list *l, int dir);
+static t_list	*ft_start_node(t_list *l, int dir);
 
 void	ft_print_stacks(t_data *data, int dir)
 {
 	t_list	*la;
 	t_list	*lb;
 
-	la = data->a->numbers;
-	lb = data->b->numbers;
+	la = ft_start_node(data->a->numbers, dir);
+	lb = ft_start_node(data->b->numbers, dir);
 	write(1, "----------", 10);
-	if (dir < 0)
-		write(1, "(rev)", 5);
 	write(1, "\n", 1);
 	while (la != NULL || lb != NULL)
 	{
@@ -48,5 +47,14 @@ static t_list	*ft_iter_direction(t_list *l, int dir)
 		return (l->next);
 	else if (dir < 0)
 		return (l->prev);
+	return (NULL);
+}
+
+static t_list	*ft_start_node(t_list *l, int dir)
+{
+	if (l == NULL || dir > 0)
+		return (l);
+	else if (dir < 0)
+		return (ft_lstlast(l));
 	return (NULL);
 }
