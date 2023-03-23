31a300bf6dc539f282827cee7854c9907b8dca75
puyma <mpuig-ma@student.42barcelona.com>
2023-03-23 17:28:52 +0100

    fixed error in strncmp

--- src/logic_bonus.c
+++ src/logic_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 17:29:39 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/22 16:08:12 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/23 15:28:07 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	ft_check_logic(t_data *data, t_list *instructions)
 		do_instruction = NULL;
 		l = l->next;
 	}
-	if (ft_issorted(data) == 0 && data->b->numbers == NULL)
+	if (ft_issorted(data) == 0 || data->b->numbers != NULL)
 		return (0);
 	return (1);
 }
@@ -47,17 +47,17 @@ static int	(*ft_set_func(char *str))(t_data *data)
 		return (&ft_sb);
 	else if (ft_strncmp(str, "ss", 2) == 0)
 		return (&ft_ss);
-	else if (ft_strncmp(str, "rra", 2) == 0)
+	else if (ft_strncmp(str, "rra", 3) == 0)
 		return (&ft_rra);
-	else if (ft_strncmp(str, "rrb", 2) == 0)
+	else if (ft_strncmp(str, "rrb", 3) == 0)
 		return (&ft_rrb);
-	else if (ft_strncmp(str, "rrr", 2) == 0)
+	else if (ft_strncmp(str, "rrr", 3) == 0)
 		return (&ft_rrr);
-	else if (ft_strncmp(str, "ra", 3) == 0)
+	else if (ft_strncmp(str, "ra", 2) == 0)
 		return (&ft_ra);
-	else if (ft_strncmp(str, "rb", 3) == 0)
+	else if (ft_strncmp(str, "rb", 2) == 0)
 		return (&ft_rb);
-	else if (ft_strncmp(str, "rr", 3) == 0)
+	else if (ft_strncmp(str, "rr", 2) == 0)
 		return (&ft_rr);
 	return (NULL);
 }
