f709435505b7051ee8cfb72bc874c466b01d62ee
puyma <mpuig-ma@student.42barcelona.com>
2023-03-23 17:28:34 +0100

    check line while input

--- src/main_bonus.c
+++ src/main_bonus.c
@@ -6,15 +6,15 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 14:56:31 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/22 17:04:35 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/23 17:25:50 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 static void		ft_delete_nl(void *ptr);
-static t_list	*ft_read_instructions(void);
-static int		ft_check_instructions(t_list *instructions);
+static int		ft_read_instructions(t_list *instructions);
+static int		ft_check_instruction(char *instruction);
 
 int	main(int argc, char **argv)
 {
@@ -24,13 +24,13 @@ int	main(int argc, char **argv)
 	if (argc < 2)
 		return (0);
 	data = ft_init_data();
+	instructions = NULL;
 	if (data == NULL || ft_parse_arguments(data, argc, argv) == 0)
 	{
 		ft_free(data);
 		ft_exit(0);
 	}
-	instructions = ft_read_instructions();
-	if (ft_check_instructions(instructions) == 0)
+	if (ft_read_instructions(instructions) == 0)
 	{
 		ft_free(data);
 		ft_exit(0);
@@ -44,21 +44,28 @@ int	main(int argc, char **argv)
 	return (0);
 }
 
-static t_list	*ft_read_instructions(void)
+static int	ft_read_instructions(t_list *instructions)
 {
 	t_list	*list;
 	char	*line;
 
 	list = NULL;
-	line = NULL;
 	line = get_next_line(STDIN_FILENO);
 	while (line != NULL && *line != '\0')
 	{
+		if (ft_check_instruction(line) == 0)
+		{
+			ft_lstclear(&list, &free);
+			return (0);
+		}
 		ft_lstadd_back(&list, ft_lstnew_str(line));
 		line = get_next_line(STDIN_FILENO);
 	}
+	if (list == NULL)
+		return (0);
 	ft_lstiter_s(list, &ft_delete_nl);
-	return (list);
+	instructions = list;
+	return (1);
 }
 
 static void	ft_delete_nl(void *ptr)
@@ -74,30 +81,23 @@ static void	ft_delete_nl(void *ptr)
 	}
 }
 
-static int	ft_check_instructions(t_list *instructions)
+static int	ft_check_instruction(char *str)
 {
-	t_list	*l;
-	char	*str;
-	size_t	len;
+	size_t		len;
 
-	l = instructions;
-	if (l == NULL)
+	ft_delete_nl(str);
+	if (str == NULL || *str == '\0')
 		return (1);
-	while (l != NULL)
-	{
-		str = l->content_s;
-		len = ft_strlen(str);
-		if (*str == 'p' && ft_strchr("ab", *(str + 1)) != 0 && len == 2)
-			return (1);
-		if (ft_strchr("sr", *str) != 0
-			&& (ft_strchr("ab", *(str + 1)) != 0
-				|| *(str + 1) == *str) && len == 2)
-			return (1);
-		else if (*str == 'r' && *(str + 1) == 'r'
-			&& (ft_strchr("ab", *(str + 2)) != 0
-				|| *(str + 2) == *str) && len == 3)
-			return (1);
-		l = l->next;
-	}
+	len = ft_strlen(str);
+	if (*str == 'p' && ft_strchr("ab", *(str + 1)) != 0 && len == 2)
+		return (1);
+	if (ft_strchr("sr", *str) != 0
+		&& (ft_strchr("ab", *(str + 1)) != 0
+			|| *(str + 1) == *str) && len == 2)
+		return (1);
+	else if (*str == 'r' && *(str + 1) == 'r'
+		&& (ft_strchr("ab", *(str + 2)) != 0
+			|| *(str + 2) == *str) && len == 3)
+		return (1);
 	return (0);
 }
