c50f048fb52328cfa9f68752136c3219c5f5ce0c
puyma <mpuig-ma@student.42barcelona.com>
2023-03-30 12:33:42 +0200

    reduced logic_mini

--- src/ft_issorted.c
+++ src/ft_issorted.c
@@ -6,17 +6,17 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 15:05:19 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/21 15:05:51 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/30 12:31:57 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_issorted(t_data *data)
+int	ft_issorted(t_stack *stack)
 {
 	t_list	*l;
 
-	l = data->a->numbers;
+	l = stack->numbers;
 	while (l != NULL && l->next != NULL)
 	{
 		l = l->next;
--- src/logic_mini.c
+++ src/logic_mini.c
@@ -6,15 +6,24 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 15:07:24 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/03/28 15:42:35 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/03/30 12:32:59 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_case_five(t_data *data)
+static void	ft_case_three(t_data *data);
+
+int	ft_do_logic_mini(t_data *data)
 {
-	if (ft_issorted(data) == 0)
+	if (ft_issorted(data->a) != 0)
+		return (0);
+	if (data->size == 2)
+	{
+		if (data->a->numbers->content > data->a->numbers->next->content)
+			ft_ra(data);
+	}
+	else if (data->size <= 5)
 	{
 		while (ft_lstsize(data->a->numbers) != 3)
 			ft_pb_smallest(data);
@@ -22,61 +31,26 @@ void	ft_case_five(t_data *data)
 		while (data->b->numbers != NULL)
 			ft_pa(data);
 	}
+	return (0);
 }
 
-void	ft_case_three(t_data *data)
+static void	ft_case_three(t_data *data)
 {
-	int	x;
-	int	y;
-	int	z;
+	int	xyz[3];
 
-	x = 0;
-	y = 0;
-	z = 0;
+	xyz[0] = 0;
+	xyz[1] = 0;
+	xyz[2] = 0;
 	if (data->a->numbers->content > data->a->numbers->next->content)
-		x = 1;
+		xyz[0] = 1;
 	if (data->a->numbers->next->content > data->a->numbers->next->next->content)
-		y = 1;
+		xyz[1] = 1;
 	if (data->a->numbers->next->next->content > data->a->numbers->content)
-		z = 1;
-	if ((x + y + z) >= 2)
+		xyz[2] = 1;
+	if ((xyz[0] + xyz[1] + xyz[2]) >= 2)
 		ft_sa(data);
-	if (y == z)
+	if (xyz[1] == xyz[2])
 		ft_ra(data);
-	if (y == 1 && z == 0)
+	if (xyz[1] == 1 && xyz[2] == 0)
 		ft_rra(data);
-}
-
-void	ft_case_two(t_data *data)
-{
-	t_list	*l;
-
-	l = data->a->numbers;
-	if (l->content > l->next->content)
-		ft_ra(data);
-}
-
-void	ft_pb_smallest(t_data *data)
-{
-	t_list	*la;
-	t_list	*smallest;
-	int		position;
-
-	la = data->a->numbers;
-	smallest = la;
-	while (la != NULL)
-	{
-		if (smallest->content > la->content)
-			smallest = la;
-		la = la->next;
-	}
-	position = ft_lst_position(data->a->numbers, smallest);
-	while (data->a->numbers->content != smallest->content)
-	{
-		if ((data->size - position) >= data->size / 2)
-			ft_ra(data);
-		else
-			ft_rra(data);
-	}
-	ft_pb(data);
 }
