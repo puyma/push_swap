1a432f67c7888ae150f816dad62598e3b56125af
puyma <mpuig-ma@student.42barcelona.com>
2023-04-15 16:43:11 +0200

    norm corrections

--- src/logic_chunks.c
+++ src/logic_chunks.c
@@ -13,6 +13,7 @@
 #include "push_swap.h"
 
 static void	ft_sort_by_chunks(t_data *data, int chunk);
+static void	ft_do_rotate(t_data *data, int chunk);
 static void	ft_sort_by_index(t_data *data);
 
 int	ft_do_chunk_method(t_data *data)
@@ -25,7 +26,6 @@ int	ft_do_chunk_method(t_data *data)
 		data->chunk_size = 60;
 	chunk = data->chunk_size;
 	ft_sort_by_chunks(data, chunk);
-	//ft_do_logic_mini(data);
 	ft_sort_by_index(data);
 	return (0);
 }
@@ -44,26 +44,27 @@ static void	ft_sort_by_chunks(t_data *data, int chunk)
 			push(data->a, data->b, data->b->id);
 			if (data->b->numbers
 				&& data->b->numbers->index < chunk - (data->chunk_size / 2))
-				{
-					if (data->a->numbers && data->a->numbers->index > chunk)
-					{
-						ft_printf("rr\n");
-						rotate(data->b, 0);
-						rotate(data->a, 0);
-					}
-					else
-						rotate(data->b, data->b->id);
-				}
+				ft_do_rotate(data, chunk);
 			i++;
 		}
 		if (i == data->chunk_size)
 		{
 			chunk += data->chunk_size;
-			//if (chunk >= data->size)
-			//	chunk = data->size - 3;
 			i = 0;
 		}
 	}
+}
+
+static void	ft_do_rotate(t_data *data, int chunk)
+{
+	if (data->a->numbers && data->a->numbers->index > chunk)
+	{
+		ft_printf("rr\n");
+		rotate(data->b, 0);
+		rotate(data->a, 0);
+	}
+	else
+		rotate(data->b, data->b->id);
 }
 
 static void	ft_sort_by_index(t_data *data)
@@ -72,13 +73,9 @@ static void	ft_sort_by_index(t_data *data)
 	{
 		ft_bring_top_biggest(data->b, data->a);
 		push(data->b, data->a, data->a->id);
-		//sort
 		if (data->a->numbers && data->a->numbers->next
 			&& data->a->numbers->next->index == data->a->numbers->index - 1)
 			swap(data->a, data->a->id);
-		//if (data->a->numbers
-		//	&& ft_lstlast(data->a->numbers)->index < data->a->numbers->index)
-		//	rev_rotate(data->a, data->a->id);
 	}
 }
 
@@ -97,14 +94,7 @@ void	ft_bring_top_biggest(t_stack *stack, t_stack *stack_a)
 	while (stack->numbers != NULL && stack->numbers != biggest)
 	{
 		func(stack, stack->id);
-
 		if (stack->numbers->index == biggest->index - 1)
 			push(stack, stack_a, stack_a->id);
-		//if (stack->numbers->index == biggest->index - 2)
-		//{
-		//	push(stack, stack_a, stack_a->id);
-		//	rotate(stack_a, stack_a->id);
-		//}
-		(void) stack_a;
 	}
 }
